735b766e961b58e05a4b6c478383b37c8de08f3e
Norly Pierre <norlypierre@gmail.com>
2025-12-17 14:54:18 -0500

    MySqlCategoryDao -> Refactor mapRow Construction,

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -124,22 +124,16 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
             throw new RuntimeException(e);
         }
     }
-    }
 
     private Category mapRow(ResultSet row) throws SQLException
     {
-        int categoryId = row.getInt("category_id");
-        String name = row.getString("name");
-        String description = row.getString("description");
 
-        Category category = new Category()
-        {{
-            setCategoryId(categoryId);
-            setName(name);
-            setDescription(description);
-        }};
+        Category category = new Category();
+
+            category.setCategoryId(row.getInt("category_id"));
+            category.setName(row.getString("name"));
+            category.setDescription(row.getString("description"));
 
         return category;
     }
-
 }
