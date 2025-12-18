fb6635fdb716d0f75002d387defea49384564250
Norly Pierre <norlypierre@gmail.com>
2025-12-18 14:29:37 -0500

    MYSqlShoppingCartDao -> Implement Cart Retrieval by User ID

---
+++ src/main/java/org/yearup/data/mysql/MYSqlShoppingCartDao.java
@@ -0,0 +1,87 @@
+package org.yearup.data.mysql;
+
+import org.springframework.stereotype.Component;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.Product;
+import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
+
+import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class MYSqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
+
+
+    public MYSqlShoppingCartDao(DataSource dataSource) {
+        super(dataSource);
+    }
+
+
+    @Override
+    public ShoppingCart getByUserId(int userId) {
+        String getByIdQuery = """
+                SELECT user_id, S.product_id, quantity, products.*
+                FROM ShoppingCartItems S JOIN Products USING (product_id)
+                WHERE user_id = ?""";
+
+        ShoppingCart cart = new ShoppingCart();
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(getByIdQuery)) {
+
+            statement.setInt(1, userId);
+
+            ResultSet row = statement.executeQuery();
+
+            while (row.next()) {
+
+                Product product = mapRow(row);
+
+                int quantity = row.getInt("quantity");
+
+                ShoppingCartItem cartItem = new ShoppingCartItem(product, quantity);
+                cart.add(cartItem);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return cart;
+    }
+
+    @Override
+    public void addItemToCart(int userId, int id) {
+
+    }
+
+    @Override
+    public void updateQuantity(int userId, int productId, int quantity) {
+
+    }
+
+    @Override
+    public void clearCart(int userId) {
+
+    }
+
+    protected static Product mapRow (ResultSet row) throws SQLException
+        {
+            int productId = row.getInt("product_id");
+            String name = row.getString("name");
+            BigDecimal price = row.getBigDecimal("price");
+            int categoryId = row.getInt("category_id");
+            String description = row.getString("description");
+            String subCategory = row.getString("subcategory");
+            int stock = row.getInt("stock");
+            boolean isFeatured = row.getBoolean("featured");
+            String imageUrl = row.getString("image_url");
+
+            return new Product(productId, name, price, categoryId, description, subCategory, stock, isFeatured, imageUrl);
+        }
+    }
