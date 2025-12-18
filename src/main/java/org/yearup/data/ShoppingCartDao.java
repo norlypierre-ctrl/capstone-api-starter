c4bfbd5d480b4cbbf1093329a9e3b1bcdb085a31
Norly Pierre <norlypierre@gmail.com>
2025-12-18 11:59:33 -0500

    ShoppingCartDao -> Additional CRUD Methods

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,15 @@
 package org.yearup.data;
 
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+    void addItemToCart(int userId, int id, ShoppingCartItem item);
+
+    void updateQuantity(int userId, int productId);
+
+    void clearCart(int userId);
 }
