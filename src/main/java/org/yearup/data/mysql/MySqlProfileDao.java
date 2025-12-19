24b927b19429f3e5d9be73bfa40f609b21b6e843
Norly Pierre <norlypierre@gmail.com>
2025-12-19 04:44:38 -0500

    MySqlProfileDao* -> Implementation with Profile CRUD & SQL Logic

--- src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
@@ -10,19 +10,40 @@ import java.sql.*;
 @Component
 public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
 {
-    public MySqlProfileDao(DataSource dataSource)
-    {
+    public MySqlProfileDao(DataSource dataSource) {
         super(dataSource);
     }
 
+    public Profile getByUserId(int userId) {
+        String userIdQuery = """
+                SELECT user_id, first_name, last_name, phone, email, address,
+                city, state, zip FROM Profiles WHERE user_id = ?
+                """;
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(userIdQuery)) {
+
+            statement.setInt(1, userId);
+
+
+            ResultSet row = statement.executeQuery();
+
+            if (row.next()) {
+                return mapRow(row);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
     @Override
-    public Profile create(Profile profile)
-    {
+    public Profile create(Profile profile) {
         String sql = "INSERT INTO profiles (user_id, first_name, last_name, phone, email, address, city, state, zip) " +
                 " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-        try(Connection connection = getConnection())
-        {
+        try (Connection connection = getConnection()) {
             PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
             ps.setInt(1, profile.getUserId());
             ps.setString(2, profile.getFirstName());
@@ -37,11 +58,55 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
             ps.executeUpdate();
 
             return profile;
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
+    @Override
+    public Profile update(int userId, Profile profile) {
+        String updateQuery = """
+                UPDATE Profiles SET first_name = COALESCE(?, first_name),
+                last_name = COALESCE(?, last_name), phone = COALESCE(?, phone),
+                email = COALESCE(?, email), address = COALESCE(?, address),
+                city = COALESCE(?, city),  state = COALESCE(?, state),
+                zip = COALESCE(?, zip) WHERE user_id = ?
+                """;
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(updateQuery)) {
+
+            statement.setString(1, profile.getFirstName());
+            statement.setString(2, profile.getLastName());
+            statement.setString(3, profile.getPhone());
+            statement.setString(4, profile.getEmail());
+            statement.setString(5, profile.getAddress());
+            statement.setString(6, profile.getCity());
+            statement.setString(7, profile.getState());
+            statement.setString(8, profile.getZip());
+            statement.setInt(9, userId);
+            statement.setInt(9, userId);
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+
+    protected static Profile mapRow(ResultSet row) throws SQLException {
+        int userId = row.getInt("user_id");
+        String firstName = row.getString("first_name");
+        String lastName = row.getString("last_name");
+        String phone = row.getString("phone");
+        String email = row.getString("email");
+        String address = row.getString("address");
+        String city = row.getString("city");
+        String state = row.getString("state");
+        String zip = row.getString("zip");
+
+        return new Profile(userId, firstName, lastName, phone, email, address,
+                city, state, zip);
+    }
 }
