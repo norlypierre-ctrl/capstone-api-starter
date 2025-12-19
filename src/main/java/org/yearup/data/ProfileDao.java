24d7bc5bcf0b1777fee56939033d9f9bdac8fc91
Norly Pierre <norlypierre@gmail.com>
2025-12-19 04:33:50 -0500

    ProfileDao* -> Method Signatures

--- src/main/java/org/yearup/data/ProfileDao.java
+++ src/main/java/org/yearup/data/ProfileDao.java
@@ -1,9 +1,10 @@
 package org.yearup.data;
 
-
 import org.yearup.models.Profile;
 
 public interface ProfileDao
 {
+    Profile getByUserId(int userId);
     Profile create(Profile profile);
+    Profile update(int userId, Profile profile);
 }
