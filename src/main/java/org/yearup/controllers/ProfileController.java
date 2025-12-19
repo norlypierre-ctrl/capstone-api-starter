1f633d5f2bfc86753356076249b65698ae76c69e
Norly Pierre <norlypierre@gmail.com>
2025-12-19 04:37:35 -0500

    ProfileController* ->  Annotations & CRUD Methods

---
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -0,0 +1,43 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.data.ProfileDao;
+import org.yearup.data.UserDao;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("profile")
+@CrossOrigin
+public class ProfileController
+{
+    private ProfileDao profileDao;
+    private UserDao userDao;
+
+    @Autowired
+    public ProfileController(UserDao userDao, ProfileDao profileDao) {
+        this.userDao = userDao;
+        this.profileDao = profileDao;
+    }
+
+    @GetMapping
+    public Profile getProfile(Principal principal){
+        String userName = principal.getName();
+        User user = userDao.getByUserName(userName);
+        int userId = user.getId();
+
+        return profileDao.getByUserId(userId);
+    }
+
+    @PutMapping
+    public Profile updateProfile(Principal principal, @RequestBody Profile profile){
+        String userName = principal.getName();
+        User user = userDao.getByUserName(userName);
+        int userId = user.getId();
+
+        return profileDao.update(userId, profile);
+    }
+}
